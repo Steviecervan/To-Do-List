8ac04a18aa7ec1da86541da0fc999ce1e106ec6a
steviecervantes <stevie.cervan06@gmail.com>
2022-08-07 20:01:23 -0700

    Project Set Up

---
+++ javascript.js
@@ -0,0 +1,17 @@
+/**
+ * To Do List
+ * 
+ * Show an unordered list of to do's
+ * Show an input to ender a new to do
+ * Show a button to add a to do.
+ * 
+ * When the button is clicked:
+ *  - The text from the input box is used to add a list item to the bottom of the list
+ *  - The text from the input box is cleared out
+ * 
+ * When the user clicks a list item, it is removed.
+ * 
+ * Extra credit: When a list item is clicked, cross it out, then remove it after 1 second
+ * 
+ * Submit GitHub Repo
+ */
